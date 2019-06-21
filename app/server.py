3820c43e5b5f0d652416059506d64d1e80736692
Siddhant Dubey <sid12.dubey@gmail.com>
2019-06-21 12:15:50 -0400

    more editing

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1meRHyDU1ZVme5NzdJotxROWnkWsl07Iq'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['sweep', 'coverdrive', 'straightdrive', 'helicopter', 'scopp', 'pull']
 path = Path(__file__).parent
 
 app = Starlette()
